42ba3eeabdd3a045e666bed6eca28d02f83a9f89
gjj2828 <gjj2828@gmail.com>
2020-05-16 12:37:02 +0800

    不同大小物体寻路的处理

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Agent : MonoBehaviour
+{
+    public GameObject target;
+
+    private NavMeshAgent mAgent;
+    private WaitForSeconds mDelay = new WaitForSeconds(1.0f);
+    private Vector3[] mTargets = new Vector3[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAgent = GetComponent<NavMeshAgent>();
+        mTargets[0] = target.transform.position;
+        mTargets[1] = transform.position;
+        StartCoroutine(Nav());
+    }
+
+    // Update is called once per frame
+    //void Update() {
+
+    //}
+
+    IEnumerator Nav() {
+        //while(true) {
+        //    transform.position = mOrigin;
+        //    yield return mDelay;
+        //    mAgent.SetDestination(mTarget);
+        //    yield return mDelay;
+        //    while(mAgent.remainingDistance > 0.01f) yield return mDelay;
+        //    yield return mDelay;
+        //}
+
+        int targetId = 0;
+        while(true) {
+            mAgent.SetDestination(mTargets[targetId]);
+            yield return mDelay;
+            while(mAgent.remainingDistance > 0.01f) yield return mDelay;
+            yield return mDelay;
+            targetId = 1 - targetId;
+        }
+    }
+}
---
+++ Assets/Scripts/NavMeshMgr.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMgr : MonoBehaviour
+{
+    public NavMeshData data;
+
+    private NavMeshDataInstance mInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mInstance = NavMesh.AddNavMeshData(data);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
